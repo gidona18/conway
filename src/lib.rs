47419bf4430d1d16422c53a8e73134bd865d7c41
Armando Herrera <mail@jellowfish.com>
2020-04-28 16:36:54 -0700

    rules

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,35 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
+#[wasm_bindgen]
 impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbours = self.live_neighbour_count(row, col);
+                let next_cell = match (cell, live_neighbours) {
+                    // Rule 1: Any live cell with fewer than two live
+                    // neighbours dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live
+                    // neighbours lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live
+                    // neighbours dies, as if caused by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 4: Any dead cell with exactly three live
+                    // neighbours becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+                };
+                next[idx] = next_cell;
+            }
+        }
+    }
+
     fn get_index(&self, row: u32, col: u32) -> usize {
         (row * self.width + col) as usize
     }
@@ -32,7 +60,7 @@ impl Universe {
         let mut cnt = 0;
         for delta_row in [self.height - 1, 0, 1].iter().clone() {
             for delta_col in [self.width - 1, 0, 1].iter().clone() {
-                if delta_row == 0 && delta_col == 0 {
+                if *delta_row == 0 && *delta_col == 0 {
                     continue;
                 }
                 let neighbour_row = (row + delta_row) % self.height;
