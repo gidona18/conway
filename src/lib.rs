17f2866d19a275f46abd888dcae1ab17bb988215
Armando Herrera <mail@jellowfish.com>
2020-04-28 16:30:35 -0700

    universe wip

--- src/lib.rs
+++ src/lib.rs
@@ -9,11 +9,38 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+impl Universe {
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    fn live_neighbour_count(&self, row: u32, col: u32) -> u8 {
+        let mut cnt = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().clone() {
+            for delta_col in [self.width - 1, 0, 1].iter().clone() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+                let neighbour_row = (row + delta_row) % self.height;
+                let neighbour_col = (col + delta_col) % self.width;
+                let idx = self.get_index(neighbour_row, neighbour_col);
+                cnt += self.cells[idx] as u8;
+            }
+        }
+        cnt
+    }
 }
