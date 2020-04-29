bd0b6f5fe18cc60614c59c7e1e8d95a0b0f35542
Armando Herrera <mail@jellowfish.com>
2020-04-29 10:02:49 -0700

    failing tests

--- tests/web.rs
+++ tests/web.rs
@@ -5,9 +5,31 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
-wasm_bindgen_test_configure!(run_in_browser);
+extern crate conway;
+use conway::Universe;
+
+
+#[cfg(test)]
+pub fn input_spaceship() -> Universe {
+    let mut universe = Universe::new(6,6);
+    universe.set_cells(&[(1,2), (2,3), (3,1), (3,2), (3,3)]);
+    universe
+}
+
+#[cfg(test)]
+pub fn expected_spaceship() -> Universe {
+    let mut universe = Universe::new(6,6);
+    universe.set_cells(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
+    universe
+}
 
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+pub fn test_tick() {
+    // create a universe with a spaceship in it
+    let mut input_universe = input_spaceship();
+    // this universe is what the previous would look like after a tick
+    let expected_universe = expected_spaceship();
+    // tick the world and see if the result is what we expected
+    input_universe.tick();
+    assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
 }
